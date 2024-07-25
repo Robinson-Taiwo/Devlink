b51f14c45cfef66684f2620d1a65aa39a5af2291
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2024-07-25 12:42:45 +0100

    Fix: corrected theme error in link form

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -19,7 +19,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-[4rem] leading-[2rem]  w-full items-center outline-none  justify-between text-darkGrey rounded-[0.667rem] border border-border pl-[1.33rem]  px-3 py-2 text-normal ring-offset-white placeholder:text-darkGrey focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus:ring-slate-300",
+      "flex h-[4rem] leading-[2rem]  w-full items-center outline-none  justify-between text-darkGrey rounded-[0.667rem] border border-border pl-[1.33rem]  px-3 py-2 text-normal ring-offset-white placeholder:text-darkGrey focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 dark:border-slate-800 dark:bg-white dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus:ring-slate-300",
       className
     )}
     {...props}
