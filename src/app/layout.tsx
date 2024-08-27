8b7e3566320e46ca5b012b5a2487f8013e6be04f
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2024-08-27 11:23:23 +0100

    added description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
 import { Inter, Instrument_Sans } from "next/font/google";
 
-
 import "./globals.css";
 
 const inter = Instrument_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Devlinks",
-  description: "A next generation web app for customizing and saving your links",
+  description:
+    "Streamline your link sharing, customization and organization with DevLinks, the powerful tool designed for developers and content creators. Easily customize and edit links, track clicks, and enhance your online presence. built by Omoyeni Taiwo Samuel (Oluwarotimi_)",
 };
 
 export default function RootLayout({
@@ -18,12 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
-
-      <body className={inter.className}>
-
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
