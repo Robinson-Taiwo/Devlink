83d18f5a194d611628829cd880642e3276a8b8d9
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2024-08-27 11:33:10 +0100

    added description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Instrument_Sans({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Devlinks",
   description:
-    "Streamline your link sharing, customization and organization with DevLinks, the powerful tool designed for developers and content creators. Easily customize and edit links, track clicks, and enhance your online presence. built by Omoyeni Taiwo Samuel (Oluwarotimi_)",
+    "DevLinks App: Streamline your link sharing, customization, and organization. This powerful tool, designed for developers and content creators, lets you easily customize and edit links, track clicks, and enhance your online presence. Built by Omoyeni Taiwo Samuel (Oluwarotimi_ )",
 };
 
 export default function RootLayout({
